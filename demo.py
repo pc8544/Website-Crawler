b98a8ea2b3c8357c64b5f3e24fb020ec9c7f7209
Pramod Choudhary <218033539+pc8544@users.noreply.github.com>
2025-07-10 09:49:22 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,13 +3,13 @@ from website_crawler_sdk import WebsiteCrawlerConfig, WebsiteCrawlerClient
 
 """
 Author: Pramod Choudhary (websitecrawler.org)
-Version: 1.0
-Date: July 9, 2025
+Version: 1.1
+Date: July 10, 2025
 """
 
 # Replace with your actual API key, target URL, and limit
 YOUR_API_KEY = "YOUR_API_KEY" #Your API key goes here
-URL = "URL" #Enter a non redirecting URL/domain with https or http
+URL = "YOUR_URL" #Enter a non redirecting URL/domain with https or http
 LIMIT = YOUR_LIMIT #Change YOUR_LIMIT 
 
 def main():
@@ -24,24 +24,29 @@ def main():
         print(f"{task_status} << task status")
         time.sleep(2)  #Wait for 2 seconds
 
+        if not task_status:
+           break
+
         if task_status:
             status = client.get_crawl_status() #get_crawl_status() method gets the crawl status
             currenturl = client.get_current_url() #get_current_url() method gets the current URL
             data = client.get_crawl_data() # get_crawl_data() method gets the structured data once crawling has completed
 
             if status:
-                print(f"Current URL:: {status}")
+                print(f"Current Status:: {status}")
 
 
             if status == "Crawling": #Crawling is one of the status
                 print(f"Current URL:: {currenturl}")
 
             if status == "Completed!":  #Completed! (with exclamation) is one of the status
-                print("Task has been completed... closing the loop")
+                print("Task has been completed... closing the loop and gettint the data...")
                 if data:
                     print(f"JSON Data:: {data}")
                     time.sleep(20)  # Give extra time for large JSON response
                     break
+            
+           
 
     print("Job over")
 
