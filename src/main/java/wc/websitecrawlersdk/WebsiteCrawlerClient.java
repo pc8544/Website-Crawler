205ae5651a600e5feb6415ca41d8b7bae45846af
Pramod Choudhary <218033539+pc8544@users.noreply.github.com>
2025-07-22 00:23:24 +0530

    Update WebsiteCrawlerClient.java

--- src/main/java/wc/websitecrawlersdk/WebsiteCrawlerClient.java
+++ src/main/java/wc/websitecrawlersdk/WebsiteCrawlerClient.java
@@ -1,28 +1,34 @@
 package wc.websitecrawlersdk;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 import org.json.JSONObject;
 
 /**
  *
  * @author Pramod Choudhary
+ * Updated on 22nd July 2025
  */
 public class WebsiteCrawlerClient {
 
     private final String basePath = "https://www.websitecrawler.org/api";
-    private final String crawlEP = "/crawl/start?";
-    private final String currentURLEP = "/crawl/currentURL?";
-    private final String crawlDataEP = "/crawl/cwdata?";
-    private final String crawlJobClearEP = "/crawl/clear?";
-    private final String crawlWaitTimeEP = "/crawl/waitTime?";
+    private final String crawlEP = "/crawl/start";
+    private final String currentURLEP = "/crawl/currentURL";
+    private final String crawlDataEP = "/crawl/cwdata";
+    private final String crawlJobClearEP = "/crawl/clear";
+    private final String crawlWaitTimeEP = "/crawl/waitTime";
+    private final String authenticateEP = "/crawl/authenticate";
+
     private ScheduledExecutorService sec;
 
     private final WebsiteCrawlerConfig cf;
@@ -34,6 +40,9 @@ public class WebsiteCrawlerClient {
     private boolean taskStarted;
     private final OkHttpClient client;
     private int waitTime;
+    private String token;
+    private String urlToCrawl;
+    private int limit;
 
     public WebsiteCrawlerClient(WebsiteCrawlerConfig cfg) {
         this.cf = cfg;
@@ -44,42 +53,6 @@ public class WebsiteCrawlerClient {
 
     public boolean getTaskStatus() {
         return this.taskStarted;
-    }
-
-    private String createCrawlWaitTimeRequest() {
-        String apiKey = "key=" + cf.getApiKey();
-        StringBuilder sbt = new StringBuilder();
-        sbt.append(basePath)
-                .append(crawlWaitTimeEP)
-                .append(apiKey);
-        return sbt.toString();
-
-    }
-
-    private String createCrawlRequest(String url, int limit) {
-        String urlParam = "url=" + url;
-        String limitParam = "&limit=" + String.valueOf(limit);
-        String apiKey = "&key=" + cf.getApiKey();
-        StringBuilder sbt = new StringBuilder();
-        sbt.append(basePath)
-                .append(crawlEP)
-                .append(urlParam)
-                .append(limitParam)
-                .append(apiKey);
-        return sbt.toString();
-
-    }
-
-    private String createcurrentURLRequest(String url) {
-        String urlParam = "url=" + url;
-        String apiKey = "&key=" + cf.getApiKey();
-        StringBuilder sbt = new StringBuilder();
-        sbt.append(basePath)
-                .append(currentURLEP)
-                .append(urlParam)
-                .append(apiKey);
-        return sbt.toString();
-
     }
 
     public String getCurrentURL() {
@@ -94,47 +67,91 @@ public class WebsiteCrawlerClient {
         return this.crawlStatus;
     }
 
-    private String createcwDataRequest(String url) {
-
-        String urlParam = "url=" + url;
-        String apiKey = "&key=" + cf.getApiKey();
-
-        StringBuilder sbt = new StringBuilder();
-        sbt.append(basePath)
-                .append(crawlDataEP)
-                .append(urlParam)
-                .append(apiKey);
-        return sbt.toString();
-
-    }
-
-    private String crawlJobClearEP(String url) {
-        String urlParam = "url=" + url;
-        String apiKey = "&key=" + cf.getApiKey();
-
-        StringBuilder sbt = new StringBuilder();
-        sbt.append(basePath)
-                .append(crawlJobClearEP)
-                .append(urlParam)
-                .append(apiKey);
-        return sbt.toString();
-
-    }
-
     private String getResponseFromAPI(String API_URL) {
         String rs = null;
-        try {
-            //System.out.println("API_URL::" + API_URL);
-            Request request = new Request.Builder()
-                    .url(API_URL)
-                    .addHeader("Accept", "application/json")
-                    .build();
+        Request request = null;
+        String murl = this.basePath + API_URL;
+        Map mp = new HashMap<>();
 
-            try (Response response = client.newCall(request).execute()) {
-                if (response != null) {
-                    ResponseBody body = response.body();
-                    if (body != null) {
-                        rs = body.string();
+        String bearer = "Bearer " + token;
+        if (API_URL.endsWith("authenticate")) {
+            mp.put("apiKey", cf.getApiKey());
+        } else if (API_URL.endsWith("currentURL")) {
+            mp.put("url", urlToCrawl);
+        } else if (API_URL.endsWith("waitTime")) {
+            mp.put("url", urlToCrawl);
+        } else if (API_URL.endsWith("start")) {
+            mp.put("url", urlToCrawl);
+            mp.put("limit", limit);
+        } else if (API_URL.endsWith("clear")) {
+            mp.put("url", urlToCrawl);
+        } else if (API_URL.endsWith("cwdata")) {
+            mp.put("url", urlToCrawl);
+        }
+
+        String mptojs = new JSONObject(mp).toString();
+        RequestBody body = RequestBody.create(
+                mptojs, MediaType.parse("application/json")
+        );
+        try {
+            switch (API_URL) {
+                case "/crawl/waitTime":
+
+                    request = new Request.Builder()
+                            .url(murl)
+                            .addHeader("Authorization", bearer)
+                            .addHeader("Accept", "application/json")
+                            .post(body)
+                            .build();
+                    break;
+                case "/crawl/start":
+                    request = new Request.Builder()
+                            .url(murl)
+                            .addHeader("Authorization", bearer)
+                            .addHeader("Accept", "application/json")
+                            .post(body)
+                            .build();
+                    break;
+                case "/crawl/cwdata":
+                    System.out.println("Entered cwdata case " + API_URL);
+                    System.out.println("murl::" + murl);
+                    System.out.println("Bearer:?:" + bearer);
+                    request = new Request.Builder()
+                            .url(murl)
+                            .addHeader("Accept", "application/json")
+                            .addHeader("Authorization", bearer)
+                            .post(body)
+                            .build();
+                    break;
+                case "/crawl/clear":
+                    request = new Request.Builder()
+                            .url(murl)
+                            .addHeader("Authorization", bearer)
+                            .addHeader("Accept", "application/json")
+                            .post(body)
+                            .build();
+                    break;
+                case "/crawl/currentURL":
+                    request = new Request.Builder()
+                            .url(murl)
+                            .addHeader("Authorization", bearer)
+                            .addHeader("Accept", "application/json")
+                            .post(body)
+                            .build();
+                    break;
+                case "/crawl/authenticate":
+                    request = new Request.Builder()
+                            .url(murl)
+                            .addHeader("Content-Type", "application/json")
+                            .post(body)
+                            .build();
+                    break;
+            }
+
+            if (request != null) {
+                try (Response response = client.newCall(request).execute()) {
+                    if (response.body() != null) {
+                        rs = response.body().string();
                     }
                 }
             }
@@ -145,24 +162,23 @@ public class WebsiteCrawlerClient {
 
     public void submitUrlToWebsiteCrawler(String url, int limit) {
 
-        this.ftr = new ScheduledFuture[2];
+        this.urlToCrawl = url;
+        this.limit = limit;
+        this.ftr = new ScheduledFuture[3];
 
-        sec = Executors.newScheduledThreadPool(2);
+        sec = Executors.newScheduledThreadPool(3);
         Runnable task0 = () -> {
-            this.taskStarted = true;
-            String wtStr = this.createCrawlWaitTimeRequest();
-            String responseFromApi = getResponseFromAPI(wtStr);
-           // System.out.println(responseFromApi + "<<waitTime");
+            String responseFromApi = getResponseFromAPI(this.crawlWaitTimeEP);
+            System.out.println(responseFromApi + "<<waitTime");
             if (responseFromApi != null) {
                 JSONObject jobj = new JSONObject(responseFromApi);
                 if (jobj.has("waitTime")) {
                     int waitingtime = jobj.getInt("waitTime");
                     if (waitingtime > 0) {
-                       // System.out.println("Received wait time.. cancelling the task");
                         this.waitTime = waitingtime;
-                        // System.out.println("DASKJSADJKSAJD::" + cwData);
-                        ftr[0].cancel(false);
-                        ftr[1] = sec.scheduleAtFixedRate(mainTask(url, limit), 0, this.waitTime, TimeUnit.SECONDS);
+                        ftr[1].cancel(false);
+                        System.out.println("Starting the main task..");
+                        ftr[2] = sec.scheduleAtFixedRate(mainTask(url, limit), 0, this.waitTime, TimeUnit.SECONDS);
 
                     }
                 }
@@ -170,19 +186,31 @@ public class WebsiteCrawlerClient {
 
         };
 
-        ftr[0] = sec.scheduleAtFixedRate(task0, 0, 2, TimeUnit.SECONDS);
+        Runnable taskm = () -> {
+            this.taskStarted = true;
+            String responseFromApi = getResponseFromAPI(this.authenticateEP);
+            if (responseFromApi != null) {
+                JSONObject jobj = new JSONObject(responseFromApi);
+                if (jobj.has("token")) {
+                    this.token = jobj.getString("token");
+                    if (token != null || !token.isBlank()) {
+                        ftr[0].cancel(false);
+                        ftr[1] = sec.scheduleAtFixedRate(task0, 0, 2, TimeUnit.SECONDS);
+
+                    }
+                }
+            }
+
+        };
+
+        ftr[0] = sec.scheduleAtFixedRate(taskm, 0, 2, TimeUnit.SECONDS);
     }
 
     private Runnable mainTask(String url, int limit) {
         Runnable task = () -> {
             try {
-               // System.out.println("Main task has begun executing");
-                String data;
-                String cuStr = this.createcurrentURLRequest(url);
-                String cwStr = this.createcwDataRequest(url);
-                String urlStr = this.createCrawlRequest(url, limit);
-                String responseFromApi = getResponseFromAPI(urlStr);
-                String crrResponse = this.getResponseFromAPI(cuStr);
+                String responseFromApi = getResponseFromAPI(this.crawlEP);
+                String crrResponse = this.getResponseFromAPI(this.currentURLEP);
                 String status = null, urlStatus = null;
 
                 if (responseFromApi != null) {
@@ -205,16 +233,16 @@ public class WebsiteCrawlerClient {
                 }
 
                 if (crawlStatus != null && crawlStatus.equals("Completed!")) {
-                    System.out.println("Cancelling the task");
-                    data = this.getResponseFromAPI(cwStr);
-                    this.cwData = data;
-                    // System.out.println("DASKJSADJKSAJD::" + cwData);
-                    ftr[1].cancel(false);
-
+                    String data = this.getResponseFromAPI(this.crawlDataEP);
+                    if (data != null) {
+                        this.cwData = data;
+                        ftr[2].cancel(false);
+                        this.taskStarted = false;
+                    }
                 }
             } catch (Exception x) {
                 if (ftr != null) {
-                    ftr[1].cancel(false);
+                    ftr[2].cancel(false);
                 }
                 this.taskStarted = false;
             }
