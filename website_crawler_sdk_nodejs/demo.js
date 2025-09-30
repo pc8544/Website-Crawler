99d517f435e24e2f53c30eaefed8f551edd3b811
Pramod Choudhary <218033539+pc8544@users.noreply.github.com>
2025-09-30 19:19:55 +0530

    Update demo.js

--- website_crawler_sdk_nodejs/demo.js
+++ website_crawler_sdk_nodejs/demo.js
@@ -1,1 +1,18 @@
+const { WebsiteCrawlerConfig, WebsiteCrawlerClient } = require('website-crawler-sdk');
 
+const config = new WebsiteCrawlerConfig('YOUR_API_KEY');
+const client = new WebsiteCrawlerClient(config);
+
+client.submitUrlToWebsiteCrawler('YOUR_URL', 'YOUR_LIMIT');
+
+const intervalId = setInterval(() => {
+  const status = client.getCrawlStatus();
+  console.log('Status:', status);
+  console.log('Current URL:', client.getCurrentURL());
+
+  if (status === 'Completed!') {
+    console.log('Crawl Data:', client.getCrawlData());
+    console.log('Job completed...');
+    clearInterval(intervalId);
+  }
+}, 3000);
